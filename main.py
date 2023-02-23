b150ea088975c10e27de344cfcf0866ce458c0d3
Mirolg <potrykusmirek@gmail.com>
2023-02-23 16:31:37 +0100

    Fetch the title of the songs from the site and put them in a list

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+
+url = 'https://www.stohitow.pl/przeboje-lat-70'
+song_list = []
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+songs = soup.select(".song-name > a")
+for song in songs:
+    song_list.append(song.text)
+
+
+print(song_list)
