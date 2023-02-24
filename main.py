3d1b17bb3d72a180916afa48e5962e01c20415d5
Mirolg <potrykusmirek@gmail.com>
2023-02-24 13:35:57 +0100

    adding a uri check loop

--- main.py
+++ main.py
@@ -22,8 +22,15 @@ sp = spotipy.Spotify(
         client_id=getenv('CLIENT_ID'),
         client_secret=getenv('CLIENT_SECRET'),
         redirect_uri="http://localhost:8888/callback",
-        cache_path='token.txt',
+        # cache_path='token.txt',
         show_dialog=True
     )
 )
+user = sp.current_user()['id']
+uri_song = []
+
+for song in songs:
+    results = sp.search(q=song)
+    uri = results["tracks"]["items"][0]["uri"]
+    uri_song.append(uri)
 
