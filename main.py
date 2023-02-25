bf88ee4c8a4a45d93f9fbbf59ced1e5126de0f44
Mirolg <potrykusmirek@gmail.com>
2023-02-25 12:14:47 +0100

    exporting functions to a separate file

--- main.py
+++ main.py
@@ -1,29 +1,9 @@
 from dotenv import load_dotenv
 from os import getenv
-import requests
-from bs4 import BeautifulSoup
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from functions import download_titles, find_uri
 load_dotenv()
-
-
-def download_titles(year: int) -> list:
-    titles_list = []
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    songs = soup.select(".song-name > a")
-    for song in songs:
-        titles_list.append(song.text)
-    return titles_list
-
-
-def find_uri(downloaded_titles: list) -> list:
-    songs_uri_list = []
-    for title in downloaded_titles:
-        result = sp.search(q=title)
-        uri = result["tracks"]["items"][0]["uri"]
-        songs_uri_list.append(uri)
-    return songs_uri_list
 
 
 selected_years = int(input("Which years are you interested in? There are 60,70,80,90 to choose from: "))
@@ -31,7 +11,7 @@ url = f'https://www.stohitow.pl/przeboje-lat-{selected_years}'
 if selected_years not in [60, 70, 80, 90]:
     print("Date entered incorrectly.")
 else:
-    titles = download_titles(selected_years)
+    titles = download_titles(year=selected_years, url_address=url)
     sp = spotipy.Spotify(
         auth_manager=SpotifyOAuth(
             scope='playlist-modify-private',
@@ -42,9 +22,11 @@ else:
             cache_path="token.txt"
         )
     )
-    uri_song = find_uri(titles)
-    user = sp.current_user()['id']
-    create_playlist = sp.user_playlist_create(user=user, name=f"{selected_years}'s Hits", public=False)
+    uri_song = find_uri(downloaded_titles=titles,spotipy=sp)
+    create_playlist = sp.user_playlist_create(
+        user=sp.current_user()['id'],
+        name=f"{selected_years}'s Hits",
+        public=False)
 
     sp.playlist_add_items(playlist_id=create_playlist['id'], items=uri_song)
 
